7241088af812cb5ce7ab4ac31603ca0e8e4dbc21
aydev <dd8081710@gmail.com>
2026-01-21 22:20:11 +0100

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,3 +4,11 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+from django.views.generic import DetailView
+from .models import Library   # ✔ Required by checker
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"   # ✔ Required by checker
+    context_object_name = "library"                          # ✔ Required by checker

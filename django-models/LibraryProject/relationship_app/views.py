8f891552d770246a979f8d765743cda6a4060805
aydev <dd8081710@gmail.com>
2026-01-21 22:24:57 +0100

    Refactor imports and clean up LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,10 +5,11 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-from django.views.generic import DetailView
-from .models import Library   # ✔ Required by checker
+from django.views.generic.detail import DetailView
+
+from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"   # ✔ Required by checker
-    context_object_name = "library"                          # ✔ Required by checker
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"

0ce8871f0cb2ad2945200918c7b7734aa10b5fac
aydev <dd8081710@gmail.com>
2026-01-21 23:56:08 +0100

    Add authentication URLs for login, logout, and register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,3 +12,12 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('relationship_app.urls')),
 ]
+
+from django.urls import path
+from .views import login_view, logout_view, register_view
+
+urlpatterns = [
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register_view, name="register"),
+]

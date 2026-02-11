6e641aa9783606e099afd933827b63ecd4f074df
Faniyi <faniyia25@gmail.com>
2026-02-11 10:45:11 +0100

    Connect api.urls to project urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -7,6 +7,15 @@ urlpatterns = [
 ]
 
 
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
+
+
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet

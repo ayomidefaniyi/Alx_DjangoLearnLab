62034ca06daf276ed50ecf02006136e365f3601a
Faniyi <faniyia25@gmail.com>
2026-02-19 15:14:31 +0100

    Add new Project

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -17,3 +17,12 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+from .models import Post
+from django import forms
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']

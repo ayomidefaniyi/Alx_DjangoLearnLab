e197d7fd2190b83eb4d6ab64370d785f27e5a1f5
Faniyi <faniyia25@gmail.com>
2026-02-22 15:07:08 +0100

    Add changes

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 # ----------------------
@@ -45,3 +46,15 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.post.pk})
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    tags = TaggableManager()   # ← ADD THIS
+
+    def __str__(self):
+        return self.title

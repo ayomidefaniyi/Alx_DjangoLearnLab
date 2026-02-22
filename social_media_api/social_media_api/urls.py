4ddc29cb2355d259facfe62c94016a48de0a3040
Faniyi <faniyia25@gmail.com>
2026-02-22 17:05:18 +0100

    Add changes in code

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
+    path('api/accounts/', include('accounts.urls')),  # existing accounts URLs
+    path('api/', include('posts.urls')),             # <-- add this line
 ]

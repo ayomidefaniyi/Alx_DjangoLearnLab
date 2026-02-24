89c8d52a736c3510ee21752d60101c7f9ec42960
Faniyi <faniyia25@gmail.com>
2026-02-24 13:03:11 +0100

    Add project changes

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,28 +1,17 @@
-from rest_framework import viewsets, permissions, filters
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    """Custom permission to allow only authors to edit/delete their own content."""
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
+class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
+    permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_queryset(self):
+        # Get users the current user is following
+        following_users = self.request.user.following.all()
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        # Return posts from followed users ordered by newest first
+        return Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')

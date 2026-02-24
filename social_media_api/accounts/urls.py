c7966412ff0d37fd22f4ae5cdd439278480051de
Faniyi <faniyia25@gmail.com>
2026-02-24 13:10:34 +0100

    Add project changes

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,17 +1,14 @@
-# accounts/urls.py
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView
+from .views import (
+    RegisterView,
+    ProfileView,
+    FollowUserView,
+    UnfollowUserView
+)
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login/', UserLoginView.as_view(), name='login'),
-]
-
-# LibraryProject/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
 ]
